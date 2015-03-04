319a9b4ba9b355999270bb626860fcb9d551a24f
Alan Plum <me@pluma.io>
2015-03-04 16:25:58 +0100

    Fixed JSON handling.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var transformTools = require('browserify-transform-tools');
 
-module.exports = transformTools.makeRequireTransform('literalify', {}, function(args, opts, cb) {
+module.exports = transformTools.makeRequireTransform('literalify', {excludeExtensions: ['json']}, function(args, opts, cb) {
   if (opts.config && args[0] in opts.config) {
     return cb(null, opts.config[args[0]]);
   } else {
